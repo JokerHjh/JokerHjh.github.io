9846971f69e18d205b766313621679a75b3b9418
jokerhe <jokerhe@futunn.com>
2020-04-06 18:44:15 +0800

    错别字

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* ===========================================================
  * sw.js
  * ===========================================================
- * Copyright 2016 @huxpro
+ * Copyright 2020 JokerHe
  * Licensed under Apache 2.0
  * service worker scripting
  * ========================================================== */
@@ -31,9 +31,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "jokerhe.com",
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
